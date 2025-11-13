6ecfbaac762466238c07ab9573ae52860eaf1ab2
Shawn Gee <sgee@renewedvision.com>
2025-11-12 20:14:23 -0800

    scroll with selection

--- Sources/Diffi/FilePicker.swift
+++ Sources/Diffi/FilePicker.swift
@@ -6,18 +6,26 @@ struct FilePicker: View {
     @Bindable var store: StoreOf<FilePickerFeature>
 
     var body: some View {
-        List(store.files, id: \.self, selection: $store.selectedFile.sending(\.userSelectedFile)) { file in
-            Text(file.path)
-                .truncationMode(.middle)
-        }
-        .navigationTitle("Files")
-        .onKeyPress(.init("k")) {
-            store.send(.navigateUp)
-            return .handled
-        }
-        .onKeyPress(.init("j")) {
-            store.send(.navigateDown)
-            return .handled
+        ScrollViewReader { proxy in
+            List(store.files, id: \.self, selection: $store.selectedFile.sending(\.userSelectedFile)) { file in
+                Text(file.path)
+                    .truncationMode(.middle)
+                    .id(file.path)
+            }
+            .navigationTitle("Files")
+            // TODO: Make keys configurable
+            .onKeyPress(.init("k")) {
+                store.send(.navigateUp)
+                return .handled
+            }
+            .onKeyPress(.init("j")) {
+                store.send(.navigateDown)
+                return .handled
+            }
+            .onChange(of: store.selectedFile) { _, selectedFile in
+                guard let selectedFile else { return }
+                proxy.scrollTo(selectedFile.path)
+            }
         }
     }
 }
